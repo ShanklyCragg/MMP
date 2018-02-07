b42d3f3d2213a56ed5e1f7ad11565bda75128887
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-02-07 12:20:28 +0000

    Started creation of "dodgeball" scene for experimentation

---
+++ TheLab2/Assets/TheLab2/Scrips/Press_Space_To_Fire.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Press_Space_To_Fire : MonoBehaviour
+{
+    //Drag in the Bullet Emitter from the Component Inspector.
+    public GameObject Bullet_Emitter;
+
+    //Drag in the Bullet Prefab from the Component Inspector.
+    public GameObject Bullet;
+
+    //Enter the Speed of the Bullet from the Component Inspector.
+    public float Bullet_Forward_Force;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //The Bullet instantiation happens here.
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+            Destroy(Temporary_Bullet_Handler, 10.0f);
+        }
+    }
+}
