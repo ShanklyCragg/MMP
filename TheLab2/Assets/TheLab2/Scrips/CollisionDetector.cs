52b9ffecef83e1fc99a83ed6c14fbac68f25d805
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-02-07 15:43:10 +0000

    Played with some scripting using desktop interaction methods, working with collision and prefabs.

---
+++ TheLab2/Assets/TheLab2/Scrips/CollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision Collided)
+    {
+        {
+            Destroy(this.gameObject); // Destroy the object this is attached to.
+        }
+    }
+
+}
