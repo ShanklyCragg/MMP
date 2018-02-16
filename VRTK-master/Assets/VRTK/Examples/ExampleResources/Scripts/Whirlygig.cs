fb9f04d82ade0175a84c9cf4801055cdb761a138
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-02-16 15:21:07 +0000

    Work on SteamVR interaction

--- VRTK-master/Assets/VRTK/Examples/ExampleResources/Scripts/Whirlygig.cs
+++ VRTK-master/Assets/VRTK/Examples/ExampleResources/Scripts/Whirlygig.cs
@@ -4,30 +4,30 @@
 
     public class Whirlygig : VRTK_InteractableObject
     {
-        float spinSpeed = 0f;
-        Transform rotator;
+        //float spinSpeed = 0f;
+        //Transform rotator;
 
-        public override void StartUsing(VRTK_InteractUse usingObject)
-        {
-            base.StartUsing(usingObject);
-            spinSpeed = 360f;
-        }
+        //public override void StartUsing(VRTK_InteractUse usingObject)
+        //{
+           // base.StartUsing(usingObject);
+            //spinSpeed = 360f;
+        //}
 
-        public override void StopUsing(VRTK_InteractUse usingObject)
-        {
-            base.StopUsing(usingObject);
-            spinSpeed = 0f;
-        }
+        //public override void StopUsing(VRTK_InteractUse usingObject)
+        //{
+            //base.StopUsing(usingObject);
+            //spinSpeed = 0f;
+        //}
 
-        protected void Start()
-        {
-            rotator = transform.Find("Capsule");
-        }
+        //protected void Start()
+        //{
+            //rotator = transform.Find("Capsule");
+        //}
 
-        protected override void Update()
-        {
-            base.Update();
-            rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
-        }
+        //protected override void Update()
+        //{
+            //base.Update();
+            //rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+        //}
     }
 }
