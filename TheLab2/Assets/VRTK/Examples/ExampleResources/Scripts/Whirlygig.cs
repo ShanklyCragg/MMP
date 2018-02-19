916687fdc94fca48ee7ee226b6919feaf01a71b3
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-02-19 17:02:24 +0000

    Scene 1 began, with room modelled and new assets

--- TheLab2/Assets/VRTK/Examples/ExampleResources/Scripts/Whirlygig.cs
+++ TheLab2/Assets/VRTK/Examples/ExampleResources/Scripts/Whirlygig.cs
@@ -4,30 +4,15 @@
 
     public class Whirlygig : VRTK_InteractableObject
     {
-        float spinSpeed = 0f;
-        Transform rotator;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
-            base.StartUsing(usingObject);
-            spinSpeed = 360f;
+            //base.StartUsing(usingObject);
         }
 
         public override void StopUsing(VRTK_InteractUse usingObject)
         {
-            base.StopUsing(usingObject);
-            spinSpeed = 0f;
-        }
-
-        protected void Start()
-        {
-            rotator = transform.Find("Capsule");
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-            rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+            //base.StopUsing(usingObject);
         }
     }
 }
