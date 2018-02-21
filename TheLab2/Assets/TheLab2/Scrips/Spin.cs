26af6900b31459a54907c6bcd69dbad39ecb5ae4
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-02-21 17:02:54 +0000

    Physics materials and custom grip function

--- TheLab2/Assets/TheLab2/Scrips/Spin.cs
+++ TheLab2/Assets/TheLab2/Scrips/Spin.cs
@@ -12,6 +12,6 @@ public class Spin : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // Rotate the object around its local X axis at 1 degree per second
-        transform.Rotate(Vector3.forward * 1);
+        transform.Rotate(Vector3.up * 1);
     }
 }
