5cd1e4c46a05f3aaeea6c0d9c28e7ffde55449f1
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-04-10 15:53:29 +0100

    Rolling environment finished

--- TheLab2/Assets/TheLab2/Scripts/ForestDeleter.cs
+++ TheLab2/Assets/TheLab2/Scripts/ForestDeleter.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForestDeleter : MonoBehaviour {
-
-    private string tagToCompare = "Forest";
+public class ForestDeleter : MonoBehaviour
+{
 
     void OnTriggerEnter(Collider col)
     {
-        //if (col.transform.root.tag == tagToCompare)
-        //{
-            Debug.Log("forestdeleter was entered!");
-            Destroy(col.gameObject);
-        //}
+        col.transform.position += new Vector3(0, 0, 100);
     }
 
 }
