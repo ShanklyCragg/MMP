7ae058cecb58f5ff1cada9a814e5f870492a46d4
Shankly Cragg <Shankly.Cragg@gmail.com>
2018-04-20 16:41:03 +0100

    Started making displaying text have inheritance. Needs finishing.

--- TheLab2/Assets/TheLab2/Scripts/CrusherTrigger.cs
+++ TheLab2/Assets/TheLab2/Scripts/CrusherTrigger.cs
@@ -9,11 +9,23 @@ public class CrusherTrigger : MonoBehaviour {
 
     private const string TagToCompare = "Coal";
 
+    /// <summary>
+    /// When an object enters the trigger, check if the object is a piece of coal
+    /// If it is, delete it, and increment the coal counter so long as it would not "overcharge" the coal counter.
+    /// </summary>
+    /// <param name="col">
+    /// Collider Component of object which triggered collider. 
+    /// </param>
     void OnTriggerEnter(Collider col)
     {
+        //Check if object is "Coal"
         if (col.transform.root.tag == TagToCompare)
         {
+
+            //REmove the coal object
             Destroy(col.gameObject);
+
+            //Check incrementing the coal wouldn't "overcharge" it
             if (GameMaster.coal < 176)
             {
                 GameMaster.coal += 5;
